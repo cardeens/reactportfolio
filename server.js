93d71932e15cc5c0961b6b35ff004c44b081c569
JJ Cardenas <jjcardenas16@hotmail.com>
2020-05-26 17:49:47 -0700

    heroku stuff

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/send', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 module.exports = router;
